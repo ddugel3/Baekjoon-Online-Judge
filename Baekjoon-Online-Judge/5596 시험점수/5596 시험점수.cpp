7142d08d2e7efccce5de01b7b5d67f40305bd048
ddugel3 <ddugel3@gmail.com>
2020-11-25 23:28:52 +0900

    Add

---
+++ Baekjoon-Online-Judge/5596 시험점수/5596 시험점수.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main()
+{
+    int arr[2][4], S = 0, T = 0;
+    for (int i = 0; i < 2; i++) 
+        for (int j = 0; j < 4; j++) 
+            scanf("%d", &arr[i][j]);
+    S = arr[0][0] + arr[0][1] + arr[0][2] + arr[0][3];
+    T = arr[1][0] + arr[1][1] + arr[1][2] + arr[1][3];
+    if (S >= T)   printf("%d", S);
+    else printf("%d", T);
+}
---
+++ Baekjoon-Online-Judge/5597 과제 안내신 분/5597 과제 안내신 분.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+int arr[32];
+int main()
+{
+    
+    for (int i = 0; i < 28; i++) {
+        int n;  scanf("%d", &n);
+        arr[n] = 1;
+    }
+    for (int i = 1; i <= 30; i++) {
+        if (arr[i] != 1)  printf("%d\n", i);
+    }
+}
